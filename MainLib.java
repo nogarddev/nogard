7730ba3f16fd7bbf25d6442dd2d68b0648441dbd
nogarddev <115876934+nogarddev@users.noreply.github.com>
2022-10-30 10:31:43 +0000

    Update MainLib.java

--- MainLib.java
+++ MainLib.java
@@ -242,12 +242,16 @@ public class MainLib  {
     public static Integer skipa = 0;
     public static String choices(Boolean threaded, String ms, Boolean skip_spaces, String args[]) {
         //         String[] array = array.fill(myArray, null);
+        System.out.println("Mainlib:245 passed");
         if (threaded == true) {
             ThreadedScanner threadedScanner = new ThreadedScanner();
+            System.out.println("Mainlib:248 passed");
             if (startedscan.equals("0")) {
+                System.out.println("Mainlib:250 passed");
                 startyouscan = "0";
                 threadedScanner.start();
             }
+            System.out.println("Mainlib:253 passed");
             threadedScanner.outputed = "0";
             threadedScanner.ms = ms;
             threadedScanner.skip_spaces = skip_spaces;
