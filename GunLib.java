42f0611250b0c9ab18a8ebeeda2914316624310e
nogarddev <115876934+nogarddev@users.noreply.github.com>
2022-11-17 20:14:51 +0000

    Update GunLib.java

--- GunLib.java
+++ GunLib.java
@@ -1,8 +1,4 @@
 public class GunLib {
-<<<<<<< HEAD
     public static String[] gun = new String[]{"gun","1","100","1","2","70","100","3","5","30","empty","bio","10","empty"};//ranged
     public static String[] knife = new String[]{"knife","1","empty","empty","empty","empty","empty","empty","empty","empty","60","bio","10","empty"};//melee
-=======
-    public static String[] gun = new String[]{"gun","1","100","1","2","70","100","3","5","30","empty","bio","10","empty"};
->>>>>>> 3e6358535d2d412dd15f1c68c9cd3f2dc26ffd0b
 }
